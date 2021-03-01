3b4e1ca1580dd86cca8e3ca684a0a54241a550eb
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-01 08:14:55 +0530

    Update _core.py

--- LEGEND/modules/_core.py
+++ LEGEND/modules/_core.py
@@ -18,7 +18,7 @@ async def Prof(event):
         return
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./julia/modules/{}.py".format(input_str)
+    the_plugin_file = "./LEGEND/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
