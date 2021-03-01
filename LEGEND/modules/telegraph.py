d98fc1835ee9c6550cc618cdcdf3a790a1568f68
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-01 11:31:15 +0530

    Update and rename Telegraph_⛄.py to telegraph.py

--- LEGEND/modules/telegraph.py
+++ LEGEND/modules/telegraph.py
@@ -7,7 +7,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-babe = "RoseLoverX"
+babe = "LEGENDX"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=babe)
 auth_url = r["auth_url"]
