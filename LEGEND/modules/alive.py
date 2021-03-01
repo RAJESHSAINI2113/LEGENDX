c6ac3c4c58277875b30f92f059509696bde7f89d
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-01 12:57:43 +0530

    Nice

--- LEGEND/modules/alive.py
+++ LEGEND/modules/alive.py
@@ -1,27 +1,25 @@
-from LEGEND import SUDO_USERS, OWNER_ID, tbot, DEV_USERS
+"""
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+                 MADE BY LEGENDX AND PROBOY
+                   CREDITS TEAMLEGEND 
+                PLEASE DON'T REMOVE CREDITS
+"""
+
+from telethon import events, Button, custom
 from LEGEND.events import register
-from telethon.tl.types import ChatBannedRights
-from telethon import events
-from telethon.tl.functions.channels import EditBannedRequest
-"""
-@register(pattern="^/alive")
-async def alive(event):
-    if event.sender_id == OWNER_ID:
-        await event.reply("I'm Alive Master")
-    elif event.sender_id in DEV_USERS:
-        await event.reply("I'm Alive Co-Master")
-    elif event.sender_id in SUDO_USERS:
-        await event.reply("I'm Alive Pro!")
-    elif event.sender_id not in SUDO_USERS:
-        await event.reply("★彡[GRAND OFFICIAL]彡★")
-"""
-@register(pattern="^/die")
-async def alive(event):
-    if event.sender_id == OWNER_ID:
-        await event.reply("Ok Master 🤐")
-    elif event.sender_id in DEV_USERS:
-        await event.reply("Hmm")
-    elif event.sender_id in SUDO_USERS:
-        await event.reply("Jnl")
-    elif event.sender_id not in SUDO_USERS:
-        await event.reply("Soon..")
+from LEGEND import tbot
+PHOTO = "https://telegra.ph/file/42f05b10e4a417eb9e623.jpg"
+@register(pattern="/alive")
+async def awake(event):
+  LEGENDX = "HELLO THIS IS GRAND OFFICIAL \n\n\n"
+  LEGENDX += "ALL SYSTEM WORKING PROPERLY\n\n"
+  LEGENDX += "GRAND OS : 3.0 LATEST\n\n"
+  LEGENDX += "NEW UPDATE SOON\n\n"
+  LEGENDX += "TELETHON : 1.19.5 LATEST\n\n\n"
+  LEGENDX += "THANKS FOR ADD ME HERE"
+  BUTTON = [[Button.url("MASTER", "https://t.me/LEGENDX22"), Button.url("ADD ME", "https://t.me/grand50_bot?startgroup=true")]]
+  BUTTON += [[Button.url("DEVLOPER", "https://t.me/proboyx"), Button.switch_inline("REPO", query="repo", same_peer=True)]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
